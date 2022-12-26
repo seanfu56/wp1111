908162a8209666ee6c8470bbf43862dac16a03dd
Sean Fu <SeanFu920506@gmail.com>
2022-12-26 17:50:59 +0800

    m

--- hw9/frontend/src/api.js
+++ hw9/frontend/src/api.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
 const API_ROOT = process.env.NODE_ENV === "production" ? "/api" : "http://localhost:4000/api";
-export default api = axios.create({ baseURL: API_ROOT });
+export default axios.create({ baseURL: API_ROOT });
